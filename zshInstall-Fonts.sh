c9534b86e2de584b08d289a2205e46b6b015498a
Jesus <mamporrazo@hotmail.com>
2021-06-05 17:14:32 +0200

    fonts

--- zshInstall-Fonts.sh
+++ zshInstall-Fonts.sh
@@ -1,5 +1,22 @@
 echo "Instalando fuentes de zsh..."
-sudo dnf install fontawesome-fonts
+mdir ~/.fonts
+
+sudo dnf install -y powerline-fonts
+
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
+cd awesome-terminal-fonts
+cp -r ./build ~/.fonts
+fc-cache -fv ~/.fonts
+
+sudo dnf install -y fontawesome-fonts
 git clone --depth=1 https://github.com/ryanoasis/nerd-fonts ./.nerd-fonts
 cd .nerd-fonts
 sudo ./install.sh
+
+
+#cd ..
+#wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+#wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+#mv PowerlineSymbols.otf ~/.local/share/fonts/
+#fc-cache -vf ~/.local/share/fonts/
+#mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
